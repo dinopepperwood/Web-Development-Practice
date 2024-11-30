61cbce0ff47fb832a64449788125c6eda20372e4
Eli Sohn <partyplaza5561@gmail.com>
2024-11-30 17:49:28 -0600

    Finished Website For Web Development Practice

---
+++ Test.js
@@ -0,0 +1,16 @@
+function Good() {
+    // Get the output element and set the text
+    const output = document.getElementById("output");
+    output.textContent = "I'm glad you like the dog!";
+}
+
+function Bad() {
+    // Get the output element and set the text
+    const output = document.getElementById("output");
+    output.textContent = "Well I Like The Dog So I'm Better!";
+}
+
+function Final(){
+    const output = document.getElementById("end");
+    output.textContent = "Well This Is The End Of The Website";
+}
